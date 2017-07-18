249092515547bc895b332be8d5899276492e83c2
Bradford Knowlton <brad@ucanbonline.net>
2017-07-17 18:53:59 -0600

    Added include Notes to display function

--- gravity-forms-view-entries.php
+++ gravity-forms-view-entries.php
@@ -53,7 +53,7 @@ function gf_view_entries_shortcode( $params ) {
 	return $entry_html;
 }
 
-function gfve_display_profile($entry, $form, $exclude_fields = null){
+function gfve_display_profile($entry, $form, $exclude_fields = null, $include_notes = false ){
 
 	// get all field labels & IDs into a single-dimensional array
 	$fields = gfve_get_fields($form);
@@ -75,6 +75,46 @@ function gfve_display_profile($entry, $form, $exclude_fields = null){
 				$last_label = $value;
 			}
 		}
+	}
+
+	if($include_notes){
+
+		$values = array();	
+    	$notes = RGFormsModel::get_lead_notes( $entry['id'] );
+    	
+    	$format = '%1$s %2$s %4$s';
+				
+		if( !isset( $separator ) || '' == $separator ){
+	        	$separator = '|';
+        	}
+				
+		if ( sizeof( $notes ) > 0 ) {
+		
+			$entry_html .= '<div class="gfve-entry-field"><span class="gfve-field-label">Notes</span>';
+
+			foreach ( $notes as $note ) {
+			
+				$username = $note->user_name;
+				$emailaddress = $note->user_email;					
+				$date = GFCommon::format_date( $note->date_created, false );
+				$content = $note->value;
+				$userid = $note->user_id;
+				$noteid = $note->id;
+				$author = get_user_by('id', $note->user_id );
+				$name = $author->first_name . ' ' . $author->last_name;
+				
+				// $values[] = sprintf($format, $date, $username, $emailaddress, $content, $userid, $noteid, $name);    
+				
+				$entry_html .= '<span class="gfve-field-val">' . $date . '</span>';
+				$entry_html .= '<span class="gfve-field-val">' . $username . '</span>';
+	        	$entry_html .= '<span class="gfve-field-val">' . $content . '</span>';
+    	    	
+			}
+
+			$entry_html .= '</div>';
+		}
+
+
 	}
 
 	if($entry_html){
@@ -139,8 +179,6 @@ function gf_view_entries_link_shortcode( $params, $content = NULL ) {
 
 }
 
-
-
 // shortcode [gf-view-entries-list]
 add_shortcode('gf-view-entries-list', 'gf_view_entries_list_shortcode');
 
@@ -149,7 +187,9 @@ function gf_view_entries_list_shortcode( $params, $content = NULL ) {
 	extract( shortcode_atts( array(
 					'form_id'		=> '',
                     'key'			=> '',
-                    'value'			=> '',                    
+                    'value'			=> '',
+                    'exclude_fields'=> array(),
+                    'include_notes'	=> false,
                 ), $params ) );
 
 	$view_list = '';
@@ -174,10 +214,14 @@ function gf_view_entries_list_shortcode( $params, $content = NULL ) {
 		// run query
     	$entries = GFAPI::get_entries($form_id, $search_criteria);
     	
+    	$form = GFAPI::get_form($form_id);
+
     	if( $entries ){
     		$view_list .= '<ul>';
     		foreach($entries as $entry){
-				$view_list .= '<li>'.$entry['id'].'</li>';;
+				$view_list .= '<li class="entry-id-'.$entry['id'].'">';
+				$view_list .= gfve_display_profile($entry, $form, $exclude_fields, $include_notes);
+				$view_list .= '</li>';;
 			}
 			$view_list .= '</ul>';
     	}
